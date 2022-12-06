4fc911306a743d4f8b42bbfc1481b6bd95574047
kalivm90 <kalivm90@gmail.com>
2022-12-06 00:48:05 -0800

    added scoreboard, popup form, and other functionality

--- scratch.js
+++ scratch.js
@@ -34,32 +34,10 @@ const Board = (() => {
 })();
 
 
-
-const AI = (() => {
-    /* Returns random unfilled field */
-    const getRandomField = () => {
-        let board = Board.getBoard()
-        while (true) {
-            let random = getRandomNum()
-            if (!board[random]) {
-                console.log(random)
-                return random
-            }
-        }
-    }
-
-    const getRandomNum = () => {
-        return Math.floor(Math.random() * 8)
-    }
-
-    return {getRandomField}
-})()
-
-
-
 const gameController = (() => {
-    const player = Player("Bill", "Simple", "X")
+    let player = Player("Anonymous", "Boi", "X")
     const computer = Player("Computer", "AI", "O")
+    let winCount = totalCount = loseCount = 0;
     currentTurn = player.getPiece()
     let turnCount = 0;
     let gameOver = false;
@@ -79,60 +57,48 @@ const gameController = (() => {
 
 
     const playRound = (field, allFields) => {
-        playerTurn(field)
-        computerTurn(allFields)
+        _playerTurn(field)
+        _computerTurn(allFields)
     }
 
-    const playerTurn = (field) => {
+    const _playerTurn = (field) => {
         const index = Number(field.target.dataset.index);
         Board.setField(index, currentTurn)
         field.target.textContent = currentTurn
-        checkForGameOver(index)
+        _checkForGameOver(index)
     }
 
-    const computerTurn = (allFields) => {
+    const _computerTurn = (allFields) => {
         if (turnCount < 8) {
-            // const index = AI.getRandomField()
-            // Board.setField(index, currentTurn)
-            // allFields[index].textContent = currentTurn
-            // checkForGameOver(index)
-            // let winning = winners()
             let winning = wins 
             let board = Board.getBoard()
             
-            // seperates all taken positions
+            // sort all filled fields
             xPos = []
             oPos = []
             board.forEach((i, index) => (i === "X") ? xPos.push(index) : (i === "O") ? oPos.push(index) : {})
 
-            // looks for x postions in wins list
+            // looks for x postions in possible wins array
             let possibleWinning = winning.filter(arr => arr.some(num => xPos.includes(num)))
 
             if (xPos.length === 1) {
-                index = AI.getRandomField();
-                Board.setField(index, currentTurn)
-                allFields[index].textContent = currentTurn
-                checkForGameOver(index)
-            } else {
-                console.log("X ", xPos)
-                console.log("O ", oPos)
-                console.log("WINNING ", possibleWinning)
-                
+                index = _getRandomField()
+            } else {                
                 let winningXMoves = []
-                // for i in possiblewinning array
+                // looks in possibleWinning to see if X is 1 move away from winning
                 for (let i = 0; i < possibleWinning.length; i++) {
-                    // start count for how many numbers are in x and winPosib
+                    // count that checks for 2 position X has that exist in winning positions
                     var count = 0;
-                    // numbers in each sub array
+                    // looping through each value in possible winning
                     for (let j = 0; j < possibleWinning[i].length; j++) {
                         currentNum = possibleWinning[i][j]
-                        // for x in xPos
+                        // looping through x's position array
                         for (let x = 0; x < xPos.length; x++) {
                             if (xPos[x] === currentNum) {
                                 count += 1;
                             }
 
-                            // if 2 items in winPos that are in Xpos
+                            // if 2 items in winPos that are in Xpos append to array
                             if (count === 2) {
                                 winningXMoves.push(possibleWinning[i])
                             }
@@ -140,51 +106,75 @@ const gameController = (() => {
                         }
                     }
                 }
-                // remove duplicates
+                // remove duplicates that are created by loops
                 winningXMoves = winningXMoves.map(JSON.stringify).filter((e,i,a) => i === a.indexOf(e)).map(JSON.parse)
-                // filter Possible winning x postions for o positions
+                // filter Possible winning X postions for O positions
                 let filteredX = winningXMoves.filter(i => !i.some(value => oPos.includes(value)))
-                // get o postitions that block x
+                // get valid O postitions that block x
                 const oOptions = filteredX.flat().filter( x => !xPos.includes(x))
                 
-                console.log(oOptions)
-
                 // if no blocking move can be found choose random field
                 if (oOptions.length === 0) {
-                    index = AI.getRandomField()
+                    index = _getRandomField()
                 } else {
                     index = oOptions[0]
                 }
 
-                // place piece
-                Board.setField(index, currentTurn)
-                allFields[index].textContent = currentTurn
-                checkForGameOver(index)
-
             }
-
-
+            // place piece
+            Board.setField(index, currentTurn)
+            allFields[index].textContent = currentTurn
+            _checkForGameOver(index)
         }
     }
-    
-    const checkForGameOver = (index) => {
-        if (winningMove(index)) {
+
+    const _checkForGameOver = (index) => {
+        if (turnCount === 8) {
             gameOver = true
-            displayController.displayMessage(`${currentTurn} wins!`)
+            totalCount += 1
+            displayController.displayMessage("Cat's Game!")
+            displayController.showReset()
+        }
+
+        if (_winningMove(index)) {
+            gameOver = true
+            displayController.displayMessage(`${currentTurn} wins!`);
+
+            if (currentTurn === player.getPiece()) {
+                if (turnCount === 8) {
+                    winCount += 1
+                } else {
+                    winCount += 1;
+                    totalCount += 1;
+                }   
+            } else {
+                if (turnCount === 8) {
+                    loseCount += 1
+                } else {
+                    loseCount += 1;
+                    totalCount += 1;
+                }
+            }
+
             displayController.showReset()
         } 
 
         turnCount += 1;
 
-        if (turnCount === 9) {
-            gameOver = true
-            displayController.displayMessage("Cat's Game!")
-            displayController.showReset()
-        }
-
         currentTurn = currentTurn === player.getPiece() ? computer.getPiece() : player.getPiece();
+        displayController.updateScoreboard(totalCount, winCount, loseCount)
 
         return; 
+    }
+
+    const _getRandomField = () => {
+        let board = Board.getBoard()
+        while (true) {
+            let random = Math.floor(Math.random() * 8)
+            if (!board[random]) {
+                return random
+            }
+        }
     }
 
     const resetGame = () => {
@@ -197,30 +187,20 @@ const gameController = (() => {
         return gameOver
     }
 
-    const winningMove = (index) => {
-        // const winners = [
-        //     /* Horizontal */
-        //     [0, 1, 2],
-        //     [3, 4, 5],
-        //     [6, 7, 8],
-        //     /* Vertical */
-        //     [0, 3, 6],
-        //     [1, 4, 7],
-        //     [2, 5, 8],
-        //     /* Diagonal */
-        //     [2, 4, 6],
-        //     [0, 4, 8]
-        // ]
-
-        //winners
-        // Long way in scratch.js
+    const _winningMove = (index) => {
         return wins.filter(row => row.includes(index))
             .some(item => item.every(
                 (piece) => Board.getField(piece) === currentTurn
             ))
     }
-    // added winners temp function
-    return { playRound, getGameOver, winningMove, resetGame}
+    
+    const _updatePlayer = (playerInfo) => {
+        player = playerInfo
+        console.log(player.getFullName())
+    }
+
+
+    return { playRound, getGameOver, resetGame, _updatePlayer}
 })();
 
 
@@ -231,7 +211,9 @@ const displayController = (() => {
     const submit = document.querySelector("#submit")
     const resetBtn = document.querySelector("#reset")
 
-    const nav = document.querySelector("nav")
+    // const nav = document.querySelector("nav")
+    const name = document.querySelector("#name-title")
+    const mainContent = document.querySelector("main")
     const form = document.querySelector(".form-container")
     const box = document.querySelectorAll(".box");
     let messageElem = document.querySelector(".message-container")
@@ -243,18 +225,18 @@ const displayController = (() => {
         }
     }));
 
-    resetBtn.onclick = () => resetBoard();
-
+    resetBtn.onclick = () => _resetBoard();
     submit.onclick = (e) => getPlayer(e);
+    login.onclick = () => _showForm();
 
     const displayMessage = (message) => {
         let text = messageElem.querySelector("#message")
         text.textContent = message
         messageElem.style.visibility = "visible"
-        hideDisplayMessage()
+        _hideDisplayMessage()
     }
 
-    const hideDisplayMessage = () => {
+    const _hideDisplayMessage = () => {
         return setTimeout(() => {
             messageElem.style.visibility = "hidden"
         }, 1500)
@@ -265,7 +247,7 @@ const displayController = (() => {
     }
 
 
-    const resetBoard = () => {
+    const _resetBoard = () => {
         Board.resetBoard()
         box.forEach(i => i.textContent = "")
         gameController.resetGame()
@@ -276,7 +258,49 @@ const displayController = (() => {
         return box[index].textContent = piece
     }
 
-    return { displayMessage, showReset, setBox}
+    const getPlayer = (e) => {
+        e.preventDefault()
+        const first = document.querySelector("#firstName").value;
+        const last = document.querySelector("#lastName").value; 
+        const piece = document.querySelector("#piece").value;
+
+        const player = Player(first, last, piece)
+        gameController._updatePlayer(player)
+        _updateName(player.getFirst())
+        _hideForm()
+    }
+
+    const _updateName = (nameInfo) => {
+        name.textContent = `Hello, ${nameInfo}`
+    }
+
+    const _showForm = () => {
+        mainContent.style.visibility = "hidden"
+        mainContent.style.display = "none"
+        form.style.visibility = "visible";
+        form.style.display = "flex"
+
+    }
+
+    const _hideForm = () => {
+        document.querySelector("#info").remove()
+        form.style.visibility = "hidden"
+        form.style.display = "none"
+        mainContent.style.visibility = "visible"
+        mainContent.style.display = "flex"
+    }
+
+    const updateScoreboard = (totalCount, winCount, loseCount) => {
+        const total = document.querySelector("#total");
+        const wins = document.querySelector("#wins");
+        const loses = document.querySelector("#loses");
+
+        total.textContent = totalCount
+        wins.textContent = winCount
+        loses.textContent = loseCount
+    }
+
+    return { displayMessage, showReset, setBox, updateScoreboard}
 
 })();
 
@@ -307,45 +331,4 @@ const displayController = (() => {
     // const hidForm = () => {
     //     form.style.visibility = "hidden"
     //     nav.style.position = "fixed"
-    // }
-
-
-
-
-    // const computerTurn = (allFields) => {
-    //     if (turnCount < 8) {
-    //         // const index = AI.getRandomField()
-    //         // Board.setField(index, currentTurn)
-    //         // allFields[index].textContent = currentTurn
-    //         // checkForGameOver(index)
-    //         // let winning = winners()
-    //         let winning = wins 
-    //         let index;
-    //         let board = Board.getBoard()
-            
-    //         // seperates all taken positions
-    //         xPos = []
-    //         oPos = []
-    //         board.forEach((i, index) => (i === "X") ? xPos.push(index) : (i === "O") ? oPos.push(index) : {})
-
-    //         // looks for x postions in wins list
-    //         let possibleWinning = winning.filter(arr => arr.some(num => xPos.includes(num)))
-    //         // checks to see if x is 1 turn away from win
-    //         const winPositions = possibleWinning.filter(i => xPos.every(value => i.includes(value)))
-    //         if (winPositions.length === 0) {
-    //             index = AI.getRandomField()
-    //         } else {
-    //             oTurn = []
-    //             for (let i = 0; i < winPositions.length; i++) {
-    //                 oTurn.push(winPositions[i].filter(item => xPos.indexOf(item) == -1))
-    //             }
-    //             // gets position that x will win with
-    //             index = (oTurn.length > 1) ? oTurn[Math.floor(Math.random(oTurn.length))][0] : oTurn[0][0]
-    //             removeWinner(winPositions[0])
-    //         }
-    //         Board.setField(index, currentTurn)
-    //         allFields[index].textContent = currentTurn
-    //         checkForGameOver(index)
-
-    //     }
     // }
